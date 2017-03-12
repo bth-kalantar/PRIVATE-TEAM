b6147d6c187be1d8a2edb90c3a40d98f7ee5aa0e
bth-kalantar <bth.kalantar@gmail.com>
2017-03-12 23:08:13 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 267785153 -- put Your ID here! <===
+local SUDO = 121624569,317866726 -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
